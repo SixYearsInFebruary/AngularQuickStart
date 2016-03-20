2c3fc00c63510df23f4c7675bfaffa0506b8aa79
SevenYearsInFebruary <549309505@qq.com>
2016-03-20 23:12:58 +0800

    Append Angular Quick Start

--- app/app.component.ts
+++ app/app.component.ts
@@ -1,7 +1,22 @@
 import {Component} from 'angular2/core';
-
+//@Component decorator
+//Component is a decorator function that takes a metadata object. The metadata tell Angular how to create and use this component.
+//We apply this function to the component class by prefixing the function with the @ sysbol and invoking it with the
+//metadata object just above the class:
+//This particular metadata object has two fields, a selector and a template.
+//The selector specifies a simple CSS selector for an HTML element that represents the component.
+//The element for this component is named my-app. Angular create and displays an instance of our AppComponent wherever it encounters a my-app elements in the host HTML.
+//The template specifies the component's companion template, written in an enhanced form of HTML that tells Angular how to render this component's view.
+//Our template is a single line of HTML announing "My first Angular App"
+//A more advanced template could contain data building to component properties and might identify other application components which have their own template. These templates
+//might identify yet other components. In this way an Angular application becomes a tree of components.
+//Component Class
+//At the bottom of the file is an empty, do-nothing class named AppComponent
+//When we're ready to build a substantive application, we can expand this class properties and application logic. Our AppComponent class is empty because we don't need it to 
+//do anything in this QuickStart.
+//We export AppComponent so that we can import it elsewhere in our application, as we'll see when we create main.ts
 @Component({
     selector: 'my-app',
-    template: '<h1>My First Angular 2 App</h1>'
+    template: '<h1>My Second Angular 2 App</h1>'
 })
 export class AppComponent { }
--- app/main.ts
+++ app/main.ts
@@ -1,4 +1,21 @@
 import {bootstrap}    from 'angular2/platform/browser';
 import {AppComponent} from './app.component';
+//We import the two things we need to launch the application.
+//1. Angular's browser bootstrap function.
+//2. The application root component, AppComponent.
+//Then we call bootstrap with AppComponent
+//Bootstrapping is platform-specific
+//Notice that we import the bootstrap function from angular2/platform/browser, not angular2/core
+//Bootstrapping isn't core because there isn't single way to bootstrap the app. True, most applications that run
+//in a browser call the bootstrap function from this history.
+//But it is possible to load a component in a different environment. We might load it on a mobile device with Apache Cordova or NativeScript.
+//We might wish to render the first page of our application on the server to import launch performance of facilitate SEO.
+//These targets require a different kind of bootstrap function that we'd import from a different library.
+//Why create a separate main.ts file?
+//The main.ts file is tiny. This is just a QuickStart. 
+//We could have folded its few lines into the app.component file and spared ourselves some complexity.
+//We'd rather demonstrate the proper way to structure an Angular application. App bootstrapping is a separate concern from presenting a view.
+//Mixing concerns creates difficulties down the road. We might launch the AppComponent in multiple environments with different bootstrapppers.
+//Testing the component is much easier if it doesn's alse try to run the entire application.
 
 bootstrap(AppComponent);
